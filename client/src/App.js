0027d19e7e48278b2d5dc7403bd71647a959173b
aaron "hyperion <75545851+hyperionao@users.noreply.github.com>
2023-10-26 14:25:09 -0400

    Integrating previous compent into App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import OutfitGenerator from "./components/OutfitGenerator";
+
+function App(){
+    return (
+        <div className="App">
+            <OutfitGenerator />
+        </div>
+    );
+}
+
+export default App;
