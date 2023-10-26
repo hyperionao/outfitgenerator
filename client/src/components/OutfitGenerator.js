042a8c3bc24411856e688238f967026bebc027db
aaron "hyperion <75545851+hyperionao@users.noreply.github.com>
2023-10-26 01:56:32 -0400

    Creating a simple React component

---
+++ client/src/components/OutfitGenerator.js
@@ -0,0 +1,14 @@
+import React, { Component} from 'react';
+
+class OutfitGenerator extends Component{
+    render() {
+        return(
+            <div>
+                <h1>Outfit Generator!</h1>
+
+            </div>
+        );
+    }
+}
+
+export default OutfitGenerator;
